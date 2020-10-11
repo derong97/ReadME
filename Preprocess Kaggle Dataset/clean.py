2f90a1236237a9cebfeb84d1ae083dac2f534d59
Loh De Rong <derong97@gmail.com>
2020-10-11 14:30:00 +0800

    Cleaned data using ast library
    Achieves the same result as parsing string literals

--- Preprocess Kaggle Dataset/clean.py
+++ Preprocess Kaggle Dataset/clean.py
@@ -1,4 +1,5 @@
 import pandas as pd
+import ast
 
 df = pd.read_csv('kindle_reviews.csv')
 df['likes'], df['dislikes'] = 0, 0
@@ -7,11 +8,9 @@ df.rename(columns={'Unnamed: 0': 'reviewID'}, inplace=True)
 
 for i in range(len(df)):
     helpful = df.at[i, 'helpful']
-    helpful = helpful[1:-1]
-    split = helpful.split(',')
-
-    df.at[i, 'likes'] = int(split[0])
-    df.at[i, 'dislikes'] = int(split[1][1:])
+    split = ast.literal_eval(helpful)
+    df.at[i, 'likes'] = split[0]
+    df.at[i, 'dislikes'] = split[1]
 
 df.drop('helpful', axis=1).to_csv('kaggle_processed.csv', sep='\t', index=False)
 print("Done")
