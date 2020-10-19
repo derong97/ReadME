710ab0a7ee7f9c19c71d5daf0fd02741a2dc60ce
huiwen99 <56622233+huiwen99@users.noreply.github.com>
2020-10-19 17:16:52 +0800

    test select and insert into mysql (local)

---
+++ server/tests/test_review_insert.py
@@ -0,0 +1,17 @@
+import MySQLdb as mdb
+
+con = mdb.connect('127.0.0.1', 'root', '', 'dbproj') # change this to your own hostname, user, password and schema
+
+with con:
+
+    cur = con.cursor()
+    cur.execute("INSERT INTO kindle (asin, overall, reviewText, reviewTime, reviewerID, reviewerName, summary, unixReviewTime) VALUES ('test_asin', 5, 'test_reviewText', curdate(), 'test_reviewerID', 'test_reviewerName', 'test_summary', UNIX_TIMESTAMP());")
+    con.commit() # need this line to write data to table
+
+    cur.execute("SELECT * FROM kindle WHERE asin='test_asin';")
+
+
+    rows = cur.fetchall()
+
+    for row in rows:
+        print(row)
---
+++ server/tests/test_review_select.py
@@ -0,0 +1,14 @@
+import MySQLdb as mdb
+
+con = mdb.connect('127.0.0.1', 'root', '', 'dbproj') # change this to your own hostname, user, password and schema
+
+with con:
+
+    cur = con.cursor()
+    cur.execute("SELECT reviewID, reviewTime, summary FROM kindle;")
+
+    rows = cur.fetchall()
+
+    for row in rows:
+        print(row)
+
