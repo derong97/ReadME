2ccf0715da62421dd458a44f6df8a91e566915f4
ktingyew <62835795+ktingyew@users.noreply.github.com>
2020-10-25 00:54:16 +0800

    Revert "Updated mongo_script.sh (It works)"
    This reverts commit 6fa358c3c0171cf479aafdd762e61791a69cd5f2.

--- automation/mongo_script.sh
+++ automation/mongo_script.sh
@@ -1,49 +1,40 @@
 #!/bin/bash
 
-# 1. Launch the EC2 instance rom the course provided AMI, 
-# 2. Allow global IP inbound connection on port 27017 through EC2 dashboard (wizard)
-# 3. SSH in and run the following lines:
-
 # Get metadata
 wget -c https://istd50043.s3-ap-southeast-1.amazonaws.com/meta_kindle_store.zip -O meta_kindle_store.zip
 sudo apt-get install unzip
 unzip meta_kindle_store.zip
+
+# Clean up
 rm -rf *.zip
 
-# Download mongoDB using instructions from https://www.digitalocean.com/community/tutorials/how-to-install-mongodb-on-ubuntu-18-04-source
-curl -fsSL https://www.mongodb.org/static/pgp/server-4.4.asc | sudo apt-key add -
-echo "deb [ arch=amd64,arm64 ] https://repo.mongodb.org/apt/ubuntu bionic/mongodb-org/4.4 multiverse" | sudo tee /etc/apt/sources.list.d/mongodb-org-4.4.list
+# Download mongoDB using instructions from https://docs.mongodb.com/manual/tutorial/install-mongodb-on-ubuntu/
+wget -qO - https://www.mongodb.org/static/pgp/server-4.4.asc | sudo apt-key add -
+echo "deb [ arch=amd64,arm64 ] https://repo.mongodb.org/apt/ubuntu xenial/mongodb-org/4.4 multiverse" | sudo tee /etc/apt/sources.list.d/mongodb-org-4.4.list
 sudo apt-get update
 sudo apt-get install -y mongodb-org
 
-# Start the MongoDB service
-sudo systemctl start mongod.service
-# Enable the MongoDB service to startup at boot (and reboots)
-sudo systemctl enable mongod
+# Install package
+sudo apt-install mongo-tools
 
-# Debug: uncomment the following line. Expected result: Successful connection.
-# mongo --eval 'db.runCommand({ connectionStatus: 1 })'
+# Start MongoDB
+sudo systemctl start mongod
 
-# Enter mongo, and create the administrative user of this MongoDB cluster.
+# Create db and collection
 mongo
-use admin
-db.createUser({ user: "admin",
-				pwd: "password", 
-				roles: [ { role: "userAdminAnyDatabase", db: "admin" }, "readWriteAnyDatabase" ] });
+use readme_mongo;
+db.createUser({ user: "admin", pwd: "password", roles: [{ role: 'readWrite', db:'readme_mongo'}] });
+db.createCollection("kindle_metadata");
+db.createCollection("users");
 exit
 
-# To enable authentication. Ppl can still connect but can't read/write without authentication
-sudo sh -c 'echo "security:\n  authorization : enabled" >> /etc/mongod.conf'
-# To enable remote connections. Methinks you need this for pymongo to work.
+# Enable security and restart MongoDB
 sudo sed -i "s,\\(^[[:blank:]]*bindIp:\\) .*,\\1 0.0.0.0," /etc/mongod.conf
+sudo sh -c 'echo "security:\n  authorization : enabled" >> /etc/mongod.conf'
 sudo service mongod restart
 
-# Load the (project) metadata into our database
-mongoimport --authenticationDatabase admin -u "admin" -p "password" -d readme_mongo -c kindle_metadata --file meta_Kindle_Store.json --legacy
+# Load metadata to localhost MongoDB
+mongoimport -d readme_mongo -c kindle_metadata --file meta_Kindle_Store.json --legacy
 
-# Remote Connection (using another shell with mongoshell installed), Use your EC2 public IP address
-# mongo "mongodb://admin@<IP address here>:27017" -p "password"
-
-# Remote Connection with PyMongo, Use your EC2 public IP address
-# client = pymongo.MongoClient("mongodb://<IP address here>:27017", username='admin', password='password')
-
+# Need to enable TCP port 27017 accessible from 0.0.0.0 as well
+# TODO: not sure why cannot connect
