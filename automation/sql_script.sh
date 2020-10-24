5378b099edc4b1b45932e8cc562ddaf811fb7288
ktingyew <62835795+ktingyew@users.noreply.github.com>
2020-10-24 22:09:30 +0800

    Update sql_script.sh

--- automation/sql_script.sh
+++ automation/sql_script.sh
@@ -16,7 +16,8 @@ sudo apt-get update
 sudo apt install python3-pip -y
 pip3 install gdown
 gdown https://drive.google.com/uc?id=1lgrBw_XDaKjlN5fFfF47P8l9Dhm8IRME
-gdown https://drive.google.com/uc?id=18zKSytgjy56nNRP8z2IsxTVga1jGSiqZ
+gdown https://drive.google.com/uc?id=15yGajHZKapMv3W_lNruOY0TwC4WLWslg
+
 
 mysql -u root
 create database if not exists readme_sql;
