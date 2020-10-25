db8089235809f9cab590e8ea343f7f1f5a7514ea
ktingyew <62835795+ktingyew@users.noreply.github.com>
2020-10-25 15:53:01 +0800

    Updated env fields

--- server/common/mongo.py
+++ server/common/mongo.py
@@ -2,13 +2,13 @@ from decouple import config
 from pymongo import MongoClient
 
 MONGO_IP = config('MONGO_IP')
-MONGO_HOST = config('MONGO_HOST')
-MONGO_DB = config('MONGO_DB')
-MONGO_USERS = config('MONGO_USERS')
+MONGO_USER = config('MONGO_USER')
 MONGO_PW = config('MONGO_PW')
+MONGO_DB = config('MONGO_DB')
+MONGO_USERS_COL = config('MONGO_USERS_COL')
 MONGO_METADATA = config('MONGO_METADATA')
 
-mongo_client = MongoClient(host=MONGO_IP, username=MONGO_USERS, password=MONGO_PW)
+mongo_client = MongoClient(host=MONGO_IP, username=MONGO_USER, password=MONGO_PW)
 mongo_db = mongo_client[MONGO_DB]
-mongo_users = mongo_db[MONGO_USERS]
+mongo_users = mongo_db[MONGO_USERS_COL]
 mongo_metadata = mongo_db[MONGO_METADATA]
