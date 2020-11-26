da782f54b1fc04ad3198f4ce55766176f997235b
Loh De Rong <derong97@gmail.com>
2020-11-27 02:05:05 +0800

    Removed metadata from cloud formation

--- automation/teardown.sh
+++ automation/teardown.sh
@@ -8,25 +8,30 @@ fi
 keyname=$(grep KeyName logs.log | sed -e 's/.*KeyName=\(\S*\).*/\1/g')
 stackname=$(grep StackName logs.log | sed -e 's/.*StackName=\(\S*\).*/\1/g')
 
-# Remove private key from local machine
-echo "Removing private key from local machine..."
-rm $keyname.pem
+### MAKE SURE CLOUD RESOURCES ARE DELETED FIRST ###
+{
+  # Delete cloud formation stack
+  echo "Removing cloud formation stack from AWS..."
+  aws cloudformation delete-stack --stack-name $stackname
 
-# Remove config.json
-echo "Removing cloud formation configs from local machine..."
-rm ./cloud_formation/config.json
+  # Remove public key from EC2
+  echo "Removing public key from AWS..."
+  aws ec2 delete-key-pair --key-name $keyname
 
-# Remove public key from EC2
-echo "Removing public key from AWS..."
-aws ec2 delete-key-pair --key-name $keyname
+  # Remove private key from local machine
+  echo "Removing private key from local machine..."
+  rm $keyname.pem
 
-# Delete cloud formation stack
-echo "Removing cloud formation stack from AWS..."
-aws cloudformation delete-stack --stack-name $stackname
+  # Remove config.json
+  echo "Removing cloud formation configs from local machine..."
+  rm ./cloud_formation/config.json
 
-# Remove logs
-echo "Removing logs from local machine..."
-rm logs.log
+  # Remove logs
+  echo "Removing logs from local machine..."
+  rm logs.log
 
-echo "Teardown completed"
-echo "🕮 Goodbye! By ReadMe 🕮"
+  echo "Teardown completed"
+  echo "🕮 Goodbye! By ReadMe 🕮"
+} || {
+  echo "Error tearing down"
+}
