e20541948059b3797be61e6eb898a7a82b6d117a
Glenda <glenda_wee@mymail.sutd.edu.sg>
2020-12-06 12:32:42 +0800

    Fixed navbar layout

--- frontend/static/react/service-worker.js
+++ frontend/static/react/service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/static/react/precache-manifest.89b929eebb60f31a6403c2f4fffba91d.js"
+  "/static/react/precache-manifest.8c391594910bbd537d46743252969ebe.js"
 );
 
 self.addEventListener('message', (event) => {
